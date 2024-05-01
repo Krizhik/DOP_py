25cb8f4c5c97c50b6f6b8a5dabeb44de7660c744
Krizhik <mihakri007@mail.ru>
2024-05-01 11:28:12 +0300

    Добавлен функционал по проверке вхождения точки в полигон

--- dop.py
+++ dop.py
@@ -23,12 +23,24 @@ def dop_of_point(sts,point):
     DOP=[HDOP,PDOP]
     return DOP
 
+def in_poligon(polygon,check_point):
+    in_polygon_check = False
+    x = check_point[0]
+    y = check_point[1]
+    for i in range(len(polygon)):
+        xp = polygon[i][0]
+        yp = polygon[i][1]
+        xp_prev = polygon[i-1][0]
+        yp_prev = polygon[i-1][1]
+        print("начало",xp,yp,"конец",xp_prev,yp_prev)
+        if (((yp <= y and y < yp_prev) or (yp_prev <= y and y < yp)) and (x > (xp_prev - xp) * (y - yp) / (yp_prev - yp) + xp)):
+            in_polygon_check = not in_polygon_check
+    return in_polygon_check
 
 num_st=int(input("Введите количество станций"))
 zonе_height=input("Высота зоны")
 zone_widtch=input("ширина зоны")
-#ввод координат станций
- 
+#ввод координат станций 
 stations =list([[20.0,0.0,30.0],[0.0,20.0,30.0],[20.0,20.0,30.0],[0.0,0.0,30.0]])
 '''
 stations = []
@@ -43,3 +55,15 @@ print(stations)
 #задание точки измерения
 op=[10,10,27]
 dop_of_point(stations,op)
+'''
+геометрию зоны будем задавать как последовательность точек являющихся вершинами многоугольника
+'''
+geometry = np.array ([[0,0],[0,20],[20,20],[20,0]])
+"""определение нахождения точки внутри очерченой вершинами области"""
+op1=[-22,12]
+#для начала найдем центр масс вершин
+'''Алгоритм использован самый простейший — трассировка луча. Последовательно проверяются все грани
+ полигона на пересечение с лучом, идущим из точки, куда кликнул пользователь. Четное количество 
+ пересечений или нет пересечений вовсе — точка за пределами полигона. Количество 
+ пересечений нечетное — точка внутри. '''
+print(in_poligon(geometry,op1))
