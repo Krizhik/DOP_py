df5640c781f0019b24103fca3107804919fd6082
Krizhik <mihakri007@mail.ru>
2024-05-09 16:53:44 +0300

    Рабочая прога для фиксированного полигона без графического отображения

--- dop.py
+++ dop.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 #Расстояние между точками
 def dist_to(point1,point2):
     R1=((point1[0]-point2[0])**2+(point1[1]-point2[1])**2+(point1[2]-point2[2])**2)**0.5
@@ -43,24 +44,24 @@ def in_poligon(polygon,check_point):
     return in_polygon_check
 
 #Вычисление среднего DOP в полигоне при текущей конфигурации станций
-def dop_of_polygone(stas,geom_):
-    geometry_=geom_.transpose()
+def dop_of_polygone(stas,geom_,begx,begy,endx,endy):
+    """geometry_=geom_.transpose()
     begx=min(np.array(geometry_)[0][:])
     begy=min(np.array(geometry_)[1][:])
     endx=max(np.array(geometry_)[0][:])
     endy=max(np.array(geometry_)[1][:])
     print ("min x = ",begx,"min y = ",begy)
     print ("max x = ",endx,"max y = ",endy)
-
+    """
     i=0
     j=0
-    zone_step=0.1#float(input("Введите точность"))
+    zone_step=1#float(input("Введите точность"))
     dop_map=np.zeros((int((endx-begx)/zone_step),int((endy-begy)/zone_step)))
     for xi in np.arange(begx,endx,zone_step):
         j=0
         for yi in np.arange(begy,endy,zone_step):
             if in_poligon(geometry,[xi,yi]):
-                dop_map[i][j]=dop_of_point(stations,[xi,yi,20])[0]
+                dop_map[i][j]=dop_of_point(stas,[xi,yi,25])[0]
             j+=1
         i+=1
     vvv=np.array(dop_map).mean()
@@ -89,31 +90,73 @@ dop_of_point(stations,op)
 '''
 геометрию зоны будем задавать как последовательность точек являющихся вершинами многоугольника
 '''
-geometry = np.array ([[0,0],[0,zonе_height],[zone_widtch,zonе_height],[zone_widtch,0]])
+geometry = np.array ([[0,0],[0,zonе_height],[zone_widtch*2,zonе_height*2],[zone_widtch,0]])
 """определение нахождения точки внутри очерченой вершинами области"""
 op1=[10,12]
 print(in_poligon(geometry,op1))     #проверка функции вхождения токи в полигон
-print(dop_of_polygone(stations,geometry))       #проверка вычисления среднего
 
-"""geometry_=geometry.transpose()
+
+#функция перебора всех возможных расстановок станций на местности для 4 станций
+geometry_=geometry.transpose()
 begx=min(np.array(geometry_)[0][:])
 begy=min(np.array(geometry_)[1][:])
 endx=max(np.array(geometry_)[0][:])
 endy=max(np.array(geometry_)[1][:])
-print ("min x = ",begx,"min y = ",begy)
-print ("max x = ",endx,"max y = ",endy)
+print(dop_of_polygone(stations,geometry,begx,begy,endx,endy))       #проверка вычисления среднего
+#шаг по зоне
+zone_stepx=endx/5#float(input("Введите точность"))
+zone_stepy=endy/5
+#минимальное расстояние между станциями
+min_distx=zone_stepx*2 - 0.00000001
+min_disty=zone_stepy*2 - 0.00000001
+counter=0 #счетчик количества итераций
+min_dop=100000 #здесь будет храниться минимальный DOP
+#station_min_dop=list([[20.0,0.0,30.0],[0.0,20.0,30.0],[20.0,20.0,30.0],[0.0,0.0,30.0]])
+#####################################
+######## ЦИКЛ ПО СЕТОЧКЕ ############
+#####################################
+#цикл для первой станции
+for xi1 in np.arange(begx,endx+0.00000001,zone_stepx):
+    stations[0][0]=xi1
+    for yi1 in np.arange(begy,endy+0.00000001,zone_stepy):
+        if(not in_poligon(geometry,[xi1,yi1])): continue
+        stations[0][1]=yi1
+        #цикл для второй станции
+        for xi2 in np.arange(begx,endx+0.00000001,zone_stepx):
+            stations[1][0]=xi2
+            for yi2 in np.arange(begy,endy+0.00000001,zone_stepy):
+                stations[1][1]=yi2
+                #если расстояние между станциями 1 и 2 меньше минимума то пропускаем итерацию
+                if(np.abs(stations[0][0]-stations[1][0])<=min_distx) and(np.abs(stations[0][1]-stations[1][1])<=min_disty): continue
+                if(not in_poligon(geometry,[xi2,yi2])): continue
+                #цикл для третей станции
+                for xi3 in np.arange(begx,endx+0.00000001,zone_stepx):
+                    stations[2][0]=xi3
+                    for yi3 in np.arange(begy,endy+0.00000001,zone_stepy):
+                        stations[2][1]=yi3
+                        if(np.abs(stations[1][0]-stations[2][0])<=min_distx) and(np.abs(stations[1][1]-stations[2][1])<=min_disty): continue
+                        if(np.abs(stations[0][0]-stations[2][0])<=min_distx) and(np.abs(stations[0][1]-stations[2][1])<=min_disty): continue
+                        if(not in_poligon(geometry,[xi3,yi3])): continue
+                        #цикл для четвертой станции
+                        for xi4 in np.arange(begx,endx+0.00000001,zone_stepx):
+                            stations[3][0]=xi4
+                            j4=0
+                            for yi4 in np.arange(begy,endy+0.00000001,zone_stepy):
+                                stations[3][1]=yi4
+                                if(np.abs(stations[0][0]-stations[3][0])<=min_distx) and(np.abs(stations[0][1]-stations[3][1])<=min_disty): continue
+                                if(np.abs(stations[1][0]-stations[3][0])<=min_distx) and(np.abs(stations[1][1]-stations[3][1])<=min_disty): continue
+                                if(np.abs(stations[2][0]-stations[3][0])<=min_distx) and(np.abs(stations[2][1]-stations[3][1])<=min_disty): continue
+                                if(not in_poligon(geometry,[xi4,yi4])): continue
+                                counter+=1
+                                m_dop=dop_of_polygone(stations,geometry,begx,begy,endx,endy)
+                                print(counter) 
+                                if m_dop<min_dop:
+                                    print(counter) 
+                                    min_dop=m_dop
+                                    station_min_dop=copy.deepcopy(stations)
 
-i=0
-j=0
-zone_step=0.1#float(input("Введите точность"))
-dop_map=np.zeros((int((endx-begx)/zone_step),int((endy-begy)/zone_step)))
-for xi in np.arange(begx,endx,zone_step):
-    j=0
-    for yi in np.arange(begy,endy,zone_step):
-        if in_poligon(geometry,[xi,yi]):
-            dop_map[i][j]=dop_of_point(stations,[xi,yi,20])[0]
-        j+=1
-    i+=1
-vvv=np.array(dop_map).mean()
-print(vvv)"""
+print(counter)
+print("Минимальный DOP:  ",min_dop)
+print("Координаты станций \n",np.array(station_min_dop))
+
 
